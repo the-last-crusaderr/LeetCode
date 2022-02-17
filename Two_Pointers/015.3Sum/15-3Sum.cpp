b785b1ae9da9cff883aa1240b2aedb513ce0f2f8
the-last-crusaderr <52620486+the-last-crusaderr@users.noreply.github.com>
2022-02-17 14:40:32 +0530

    documenting the codebase

--- Two_Pointers/015.3Sum/15-3Sum.cpp
+++ Two_Pointers/015.3Sum/15-3Sum.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) 
     {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>>result;
+        sort(nums.begin(), nums.end());                           // sort the input vector
+        vector<vector<int>>result;                             
         
         for (int i=0; i<nums.size(); i++)
             cout<<nums[i]<<" ";
         cout<<endl;
         
-        for (int a=0; a<nums.size(); a++)
+        for (int a=0; a<nums.size(); a++)                             // fix one number one by one
         {
 
-            int sum=-nums[a];
+            int sum=-nums[a];                                         // remove the part of first number from that 
             
-            int left=a+1;
-            int right=nums.size()-1;
+            int left=a+1;                                             // left = a + 1 
+            int right=nums.size()-1;                                  // right = nums.size() - 1
+            
+            
+            // use 2 pointer method from here
             
             while (left<right)
             {
