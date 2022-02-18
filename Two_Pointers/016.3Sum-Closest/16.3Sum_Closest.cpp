a7c84f399e1039b164847cd6fc1267fdb7901aa5
the-last-crusaderr <52620486+the-last-crusaderr@users.noreply.github.com>
2022-02-18 11:07:34 +0530

    Update 16.3Sum_Closest.cpp

--- Two_Pointers/016.3Sum-Closest/16.3Sum_Closest.cpp
+++ Two_Pointers/016.3Sum-Closest/16.3Sum_Closest.cpp
@@ -4,22 +4,30 @@ public:
     {
         sort(nums.begin(),nums.end());
         
-        int temp = INT_MAX;
-        int result;
+        int temp = INT_MAX;                                     // take max for highest difference
+        int result;       
         
         for (int a=0; a<nums.size(); a++)
         {
-            int left = a+1;
-            int right = nums.size()-1;
-            int sum = target - nums[a];
+            int left = a+1;                                      // left pointer
+            int right = nums.size()-1;                           // right pointer
+            int sum = target - nums[a];                          // target composed of 3 elements, remove one from it and find the sum 
+                                                                    // in remaining pairs                        
             
+            
+            // 2 pointer method
             while (left<right)
             {
+                // find the value closest to target
+                
                 if (temp > abs(nums[left]+nums[right]-sum))
                 {
                     temp = abs(nums[left]+nums[right]-sum);
                     result = nums[a]+nums[left]+nums[right];
                 }
+                
+                // find the target and move pointer accordingly
+                
                 if (nums[left]+nums[right]==sum)
                     return target;
                 else if (nums[left]+nums[right]>sum)
